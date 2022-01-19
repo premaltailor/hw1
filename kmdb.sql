441d65dc8cc63bbde93c4d1237711441c3193f96
premaltailor <premal.tailor@kellogg.northwestern.edu>
2022-01-19 22:08:26 +0000

    initial set up

--- kmdb.sql
+++ kmdb.sql
@@ -68,9 +68,13 @@
 
 -- Drop existing tables, so you'll start fresh each time this script is run.
 -- TODO!
+DROP TABLE IF EXISTS movies;
+DROP TABLE IF EXISTS top cast;
 
 -- Create new tables, according to your domain model
 -- TODO!
+
+
 
 -- Insert data into your database that reflects the sample data shown above
 -- Use hard-coded foreign key IDs when necessary
