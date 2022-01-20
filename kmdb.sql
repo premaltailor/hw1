b5ced2fd8c247ab939e83bb149fd0f4756272322
premaltailor <premal.tailor@kellogg.northwestern.edu>
2022-01-20 20:11:36 +0000

    Fix formatting

--- kmdb.sql
+++ kmdb.sql
@@ -97,6 +97,23 @@ VALUES ("Batman Begins", 2005, "PG-13", "Christopher Nolan"),
        ("The Dark Knight", 2008, "PG-13", "Christopher Nolan"), 
        ("The Dark Knight Rises", 2012, "PG-13", "Christopher Nolan");
 
+INSERT INTO top_cast (movie_title, actor, movie_character)
+VALUES ("Batman Begins", "Christian Bale", "Bruce Wayne"), 
+       ("Batman Begins", "Michael Caine", "Alfred"),
+       ("Batman Begins", "Liam Neeson", "Ra's Al Ghul"),
+       ("Batman Begins", "Katie Holmes", "Rachel Dawes"),
+       ("Batman Begins", "Gary Oldman", "Commissioner Gordon"),
+       ("The Dark Knight", "Christian Bale", "Bruce Wayne"),
+       ("The Dark Knight", "Heath Ledger", "Joker"),
+       ("The Dark Knight", "Aaron Eckhart", "Harvey Dent"),
+       ("The Dark Knight", "Michael Caine", "Alfred"),
+       ("The Dark Knight", "Maggie Gyllenhaal", "Rachel Dawes"),
+       ("The Dark Knight Rises", "Christian Bale", "Bruce Wayne"),
+       ("The Dark Knight Rises", "Gary Oldman", "Commissioner Gordon"),
+       ("The Dark Knight Rises", "Tom Hardy", "Bane"),
+       ("The Dark Knight Rises", "Joseph Gordon-Levitt", "John Blake"),
+       ("The Dark Knight Rises", "Anne Hathaway", "Selina Kyle");
+
 -- Prints a header for the movies output
 .print "Movies"
 .print "======"
@@ -116,22 +133,5 @@ SELECT * FROM movies;
 
 -- The SQL statement for the cast output
 -- TODO!
-INSERT INTO top_cast (movie_title, actor, movie_character)
-VALUES ("Batman Begins", "Christian Bale", "Bruce Wayne"), 
-       ("Batman Begins", "Michael Caine", "Alfred"),
-       ("Batman Begins", "Liam Neeson", "Ra's Al Ghul"),
-       ("Batman Begins", "Katie Holmes", "Rachel Dawes"),
-       ("Batman Begins", "Gary Oldman", "Commissioner Gordon"),
-       ("The Dark Knight", "Christian Bale", "Bruce Wayne"),
-       ("The Dark Knight", "Heath Ledger", "Joker"),
-       ("The Dark Knight", "Aaron Eckhart", "Harvey Dent"),
-       ("The Dark Knight", "Michael Caine", "Alfred"),
-       ("The Dark Knight", "Maggie Gyllenhaal", "Rachel Dawes"),
-       ("The Dark Knight Rises", "Christian Bale", "Bruce Wayne"),
-       ("The Dark Knight Rises", "Gary Oldman", "Commissioner Gordon"),
-       ("The Dark Knight Rises", "Tom Hardy", "Bane"),
-       ("The Dark Knight Rises", "Joseph Gordon-Levitt", "John Blake"),
-       ("The Dark Knight Rises", "Anne Hathaway", "Selina Kyle");
-
 SELECT * FROM top_cast;
 
